360b862aa6370d0002434a14c8ffd06e9026fcb4
raknage <raknage@raknage.com>
2025-01-20 13:28:06 +0200

    add textnode_to_htmlnode and tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
+from htmlnode import LeafNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -8,7 +9,7 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
-        
+
     def test_eq2(self):
         node = TextNode("This is a text node", TextType.LINK, "www.url.com")
         node2 = TextNode("This is a text node", TextType.LINK, "www.url.com")
@@ -33,6 +34,41 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text", TextType.CODE)
         node2 = TextNode("This is a different text", TextType.CODE)
         self.assertNotEqual(node, node2)
+
+
+class Test_textnode_to_htmlnode(unittest.TestCase):
+    def test_textnode_to_htmlnode(self):
+        text = "This is some text!"
+        self.assertEqual(
+            TextNode(text, TextType.NORMAL).textnode_to_htmlnode().to_html(),
+            LeafNode(None, text).to_html(),
+        )
+        self.assertEqual(
+            TextNode(text, TextType.BOLD).textnode_to_htmlnode().to_html(),
+            LeafNode("b", text).to_html(),
+        )
+        self.assertEqual(
+            TextNode(text, TextType.ITALIC).textnode_to_htmlnode().to_html(),
+            LeafNode("i", text).to_html(),
+        )
+        self.assertEqual(
+            TextNode(text, TextType.CODE).textnode_to_htmlnode().to_html(),
+            LeafNode("code", text).to_html(),
+        )
+        self.assertEqual(
+            TextNode(text, TextType.LINK, "http://www.raknage.com")
+            .textnode_to_htmlnode()
+            .to_html(),
+            LeafNode("a", text, {"href": "http://www.raknage.com"}).to_html(),
+        )
+        self.assertEqual(
+            TextNode(text, TextType.IMAGE, "http://www.raknage.com")
+            .textnode_to_htmlnode()
+            .to_html(),
+            LeafNode(
+                "img", "", {"src": "http://www.raknage.com", "alt": text}
+            ).to_html(),
+        )
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 
 class TextType(Enum):
@@ -25,3 +26,20 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+    def textnode_to_htmlnode(self):
+        match self.text_type:
+            case TextType.NORMAL:
+                return LeafNode(None, self.text)
+            case TextType.BOLD:
+                return LeafNode("b", self.text)
+            case TextType.ITALIC:
+                return LeafNode("i", self.text)
+            case TextType.CODE:
+                return LeafNode("code", self.text)
+            case TextType.LINK:
+                return LeafNode("a", self.text, {"href": self.url})
+            case TextType.IMAGE:
+                return LeafNode("img", "", {"src": self.url, "alt": self.text})
+            case _:
+                raise ValueError("Invalid TextType")
