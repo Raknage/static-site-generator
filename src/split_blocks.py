f64ee93bc4ba717603c9d61cac105d9a4a9f1698
raknage <raknage@raknage.com>
2025-02-04 01:28:40 +0200

    add block_to_blocktype and tests

--- src/split_blocks.py
+++ src/split_blocks.py
@@ -1,3 +1,21 @@
+import re
+
+
 def markdown_to_blocks(markdown):
     blocks = [block.strip() for block in markdown.split("\n\n")]
     return [x for x in blocks if x]
+
+
+def block_to_blocktype(block):
+    if re.fullmatch(r"^#{1,6} .{1,}", block):
+        return "heading"
+    elif re.fullmatch(r"^`{3}.*`{3}$", block, re.DOTALL):
+        return "code"
+    elif re.fullmatch(r"^>.*(?:\n>.*|)*$", block, re.MULTILINE):
+        return "quote"
+    elif re.fullmatch(r"^(\*|\+|-) \w+.*(?:\n(\*|\+|-) \w+.*)*$", block, re.MULTILINE):
+        return "unordered_list"
+    elif re.fullmatch(r"^\d+\. .+\n?(\d+\. .+\n?)*$", block, re.MULTILINE):
+        return "ordered_list"
+    else:
+        return "paragraph"
--- src/test_split_blocks.py
+++ src/test_split_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from split_blocks import markdown_to_blocks
+from split_blocks import markdown_to_blocks, block_to_blocktype
 
 
 class Test_(unittest.TestCase):
@@ -24,3 +24,18 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
                 "* This is the first list item in a list block\n* This is a list item\n* This is another list item",
             ],
         )
+
+    def test_block_to_blocktype(self):
+        header = "###### this is a header"
+        code = """```this is\ncode```"""
+        quote = """> this\n> is\n> a\n> quote"""
+        u_list = """* list item 1\n* list item 2\n- list item 3"""
+        o_list = """1. list item 1\n1. list item 2\n3. list item 3"""
+        paragraph = "this is just a\nnormal paragraph"
+
+        self.assertEqual(block_to_blocktype(header), "heading")
+        self.assertEqual(block_to_blocktype(code), "code")
+        self.assertEqual(block_to_blocktype(quote), "quote")
+        self.assertEqual(block_to_blocktype(u_list), "unordered_list")
+        self.assertEqual(block_to_blocktype(o_list), "ordered_list")
+        self.assertEqual(block_to_blocktype(paragraph), "paragraph")
